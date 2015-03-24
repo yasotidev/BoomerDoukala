d190d4d0ff414a8ab39bb82f5be57c8c0273cb90
YasotiDev <yasotidev@outlook.fr>
2015-03-24 22:52:56 +0100

    Fix commit.

--- DoukalaTemplate/Src/Client/www/Web/Doukala/Models/Compagny.cs
+++ DoukalaTemplate/Src/Client/www/Web/Doukala/Models/Compagny.cs
@@ -1,6 +1,6 @@
 ﻿namespace Doukala.Models
 {
-    public class Compagny : DomainObject
+    public class Compagny 
     {
         public byte[] LogoAvatar { get; set; }
         public string Logo { get; set; }
