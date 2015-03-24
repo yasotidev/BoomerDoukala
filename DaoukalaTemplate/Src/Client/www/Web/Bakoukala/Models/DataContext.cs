6de3a462a2c1545e3edae9ad7cf92391ebe12c9c
YasotiDev <yasotidev@outlook.fr>
2015-03-24 21:46:41 +0100

    Start preparing project

---
+++ DaoukalaTemplate/Src/Client/www/Web/Bakoukala/Models/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Bakoukala.Models
+{
+    public class DataContext<TContext> : DbContext where TContext : DbContext
+    {
+        static DataContext()
+        {
+            Database.SetInitializer<TContext>(null);
+        }
+
+        public DataContext(bool proxyCreation = true)
+            :base("name=Doukala")
+        {
+            Configuration.ProxyCreationEnabled = proxyCreation;
+        }
+    }
+}
