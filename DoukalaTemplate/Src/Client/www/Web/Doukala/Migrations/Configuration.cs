e368f2d25c71a8156e4925a625ad8e482f40122c
YasotiDev <yasotidev@outlook.fr>
2015-03-25 00:29:53 +0100

    Update-Database commit.

--- DoukalaTemplate/Src/Client/www/Web/Doukala/Migrations/Configuration.cs
+++ DoukalaTemplate/Src/Client/www/Web/Doukala/Migrations/Configuration.cs
@@ -1,18 +1,17 @@
+using Doukala.Models;
+
 namespace Doukala.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Models.DefaultContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<DefaultContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(Models.DefaultContext context)
+        protected override void Seed(DefaultContext context)
         {
             //  This method will be called after migrating to the latest version.
 
