5ce66d71a3bc4a346e8e9eea5987bd2e01dc532a
YasotiDev <yasotidev@outlook.fr>
2015-03-24 22:45:45 +0100

    Rename Project a refract commit.

---
+++ DoukalaTemplate/Src/Client/www/Web/Doukala/App_Start/BundleConfig.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Web.Optimization;
+
+namespace Doukala
+{
+    public class BundleConfig
+    {
+        // Pour plus d’informations sur le regroupement, rendez-vous sur http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            
+        }
+    }
+}
---
+++ DoukalaTemplate/Src/Client/www/Web/Doukala/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+
+namespace Doukala
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuration et services Web API
+            config.EnableSystemDiagnosticsTracing();
+            // Configurez Web API pour utiliser uniquement l’authentification par jeton de support.
+            // Utilisez la casse mixte pour les données JSON.
+
+            // Routes Web API
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
